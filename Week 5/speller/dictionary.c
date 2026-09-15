8e6d1a145f79ce403aece8e730c8077c21266bd9
Onur Karataş <108953583+onurkaratass@users.noreply.github.com>
2026-09-16 00:51:01 +0300

    Add files via upload

---
+++ Week 5/speller/dictionary.c
@@ -0,0 +1,106 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+const unsigned int N = 100003;
+
+node *table[N];
+
+unsigned int total_words = 0;
+
+bool check(const char *word)
+{
+    unsigned int index = hash(word);
+    node *cursor = table[index];
+
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+
+    return false;
+}
+
+unsigned int hash(const char *word)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = tolower(*word++)))
+    {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash % N;
+}
+
+bool load(const char *dictionary)
+{
+    FILE *source = fopen(dictionary, "r");
+    if (source == NULL)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(source, "%45s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            fclose(source);
+            return false;
+        }
+
+        strcpy(n->word, word);
+        unsigned int index = hash(word);
+
+        n->next = table[index];
+        table[index] = n;
+
+        total_words++;
+    }
+
+    fclose(source);
+    return true;
+}
+
+unsigned int size(void)
+{
+    return total_words;
+}
+
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+    }
+    return true;
+}
